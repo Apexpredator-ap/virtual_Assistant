9890e6eea607c2e9c2438512656ac05041078e6b
abhij <abhiramm95@gmail.com>
2024-02-02 20:48:07 +0530

    Initial commit

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        toolbarHeight: 70,
+        title: Text("JARVIS",style :GoogleFonts.aBeeZee(fontSize: 28,color: Colors.grey),),
+        // centerTitle: true,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 77, 234, 255),
+                Color.fromARGB(172, 30, 148, 233)
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+        ),
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
+    );
+  }
+}
